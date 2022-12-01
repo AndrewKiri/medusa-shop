f41483bf56b282873ea0d07ac127cbdcbafa30cc
andrewkiri <andrewkirichok@gmail.com>
2022-12-01 15:50:29 +0100

    feat: add prettier, tailwind && medusa package

---
+++ components/ProductCard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductCard = () => {
+  return <div>product name</div>;
+};
+
+export default ProductCard;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react";
+import { QueryClient } from "react-query";
+import { MedusaProvider } from "medusa-react";
+
+// Your react-query's query client config
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 30000,
+      retry: 1,
+    },
+  },
+});
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MedusaProvider
+      queryClientProviderProps={{ client: queryClient }}
+      baseUrl="http://localhost:9000"
+    >
+      <Component {...pageProps} />
+    </MedusaProvider>
+  );
 }
